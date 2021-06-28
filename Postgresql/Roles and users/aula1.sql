1d4d6382a2f4f2520ba5b5f3283f65ba0bd85076
Carol Manso <carol.manso1@gmail.com>
2021-06-28 02:35:17 -0300

    aula users and roles

---
+++ Postgresql/Roles and users/aula1.sql
@@ -0,0 +1,18 @@
+-- ROLES, USERS --
+CREATE ROLE professores NOCREATEDB NOCREATEROLE INHERIT NOLOGIN NOBYPASSRLS CONNECTION LIMIT 10;
+ALTER ROLE professores PASSWORD '123'; 
+CREATE ROLE Cintia LOGIN PASSWORD '123'; 
+--Excluindo a role cintia e recriando com outras configurações--
+DROP ROLE cintia;
+CREATE ROLE Cintia LOGIN PASSWORD '123' IN ROLE professores; --Cintia faz parte da role professores--
+DROP ROLE Cintia; 
+CREATE --ROLE Cintia LOGIN PASSWORD '123' ROLE professores; Agora, professores faz parte de Cintia--
+DROP ROLE Cintia; 
+--Trabalhando com revoke e grant--
+CREATE TABLE teste (nome varchar); 
+GRANT ALL ON TABLE teste to professores; 
+CREATE ROLE Cintia LOGIN PASSWORD '123'; 
+DROP ROLE Cintia; 
+CREATE ROLE Cintia INHERIT LOGIN PASSWORD '123' IN ROLE professores;
+REVOKE professores FROM cintia;
+
