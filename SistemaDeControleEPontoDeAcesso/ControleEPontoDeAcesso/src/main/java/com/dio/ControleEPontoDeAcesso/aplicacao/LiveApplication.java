103ba0bba3917d69aa108706736f2fccdd1e4857
Carol Manso <carol.manso1@gmail.com>
2021-07-11 18:28:53 -0300

    mudando o nome do repositorio

--- SistemaDeControleEPontoDeAcesso/ControleEPontoDeAcesso/src/main/java/com/dio/ControleEPontoDeAcesso/aplicacao/LiveApplication.java
+++
@@ -1,9 +0,0 @@
-package com.dio.ControleEPontoDeAcesso.aplicacao;
-
-import org.springframework.boot.SpringApplication;
-
-public class LiveApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(LiveApplication.class, args);
-    }
-}
