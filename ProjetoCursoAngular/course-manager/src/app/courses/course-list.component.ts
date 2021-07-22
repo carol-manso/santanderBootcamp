405d277ffa11109986de1c9c30b9073e3e953397
Carol Manso <carol.manso1@gmail.com>
2021-07-22 14:47:45 -0300

    parte 2 do projeto- adicionando opcoes de edicao

--- ProjetoCursoAngular/course-manager/src/app/courses/course-list.component.ts
+++ ProjetoCursoAngular/course-manager/src/app/courses/course-list.component.ts
@@ -1,43 +1,32 @@
 import { OnInit } from "@angular/core";
 import { Component } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 
 @Component({
-    selector : 'app-course-list', 
+ 
     templateUrl:  './course-list.component.html'
 })
 export class CourseListComponent implements OnInit{
-    courses: Course[] = [];
+    _courses: Course[] = [];
+    filteredCourses : Course[] = [];
     
-   
+    _filterBy : string = '';
+    constructor(private courseService : CourseService){}
     
 
     ngOnInit(): void { 
-        this.courses = [
-            {
-                id : 1, 
-                name : 'Angular Forms', 
-                imageUrl : '', 
-                price : 99.99, 
-                duration : 120, 
-                rating : 4.5, 
-                description : 'Curso dedicado ao ensino de Angular Forms', 
-                releaseDate: 'December 2, 2020',
-                code: 'XPS- 87684', 
-            }, 
-            {
-                id : 2, 
-                name : 'Angular HTTP', 
-                imageUrl : '', 
-                price : 90.99, 
-                duration : 125, 
-                rating : 4, 
-                description : 'Curso dedicado ao ensino de Angular HTTP', 
-                releaseDate: 'November 4, 2020',
-                code: 'XTR- 89984', 
-            }
+       this._courses = this.courseService.retrieveAll();
+        this.filteredCourses = this._courses;
+    }
 
-        ]
+    set filter(value: string){
+        this._filterBy = value;
+        this.filteredCourses = this._courses.filter((course:Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+        
+    }
+    get filter(){
+        return this._filterBy;
     }
 }
